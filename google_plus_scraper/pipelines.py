ffce75d52f438e380fd10a06a4cd93623b9e9f4a
taemyung <tmheo74@gmail.com>
2016-03-09 00:12:02 +0900

    #2 implemented save scraped item to db
    save scraped item to sqlite using scrape pipeline

--- google_plus_scraper/pipelines.py
+++ google_plus_scraper/pipelines.py
@@ -4,8 +4,52 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import sqlite3
+import os
+con = None
 
 class GooglePlusScraperPipeline(object):
+    def __init__(self):
+        self.setupDBCon()
+        self.createTables()
+        
+    def setupDBCon(self):
+        self.con = sqlite3.connect(os.getcwd() + '/google_plus_scraper.db')
+        self.cur = self.con.cursor()
+    
+    def createTables(self):
+        self.createTable()
+    
+    def closeDB(self):
+        self.con.close()
+        
+    def __del__(self):
+        self.closeDB()
+        
+    def createTable(self):
+        self.cur.execute("CREATE TABLE IF NOT EXISTS google_plus_scraper_items(id INTEGER PRIMARY KEY NOT NULL, \
+            title TEXT, \
+            link TEXT, \
+            date TEXT \
+            )")
+    
     def process_item(self, item, spider):
+        self.storeInDb(item)
         return item
+
+    def storeInDb(self,item):
+        self.cur.execute("INSERT INTO google_plus_scraper_items(\
+            title, \
+            link, \
+            date \
+            ) \
+        VALUES( ?, ?, ?)", \
+        ( \
+            item.get('title',''),
+            item.get('link',''),
+            item.get('date','')
+        ))
+        print '------------------------'
+        print 'Data Stored in Database'
+        print '------------------------'
+        self.con.commit()
