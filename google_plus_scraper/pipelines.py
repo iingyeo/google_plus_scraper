c73bca296ffd6a102df6c627346c0db30400e0c1
taemyung <tmheo74@gmail.com>
2016-03-11 23:08:19 +0900

    #5 implement not to save duplicate scraped item
    - add unique index to link column
    _ handle integrityerror

--- google_plus_scraper/pipelines.py
+++ google_plus_scraper/pipelines.py
@@ -6,6 +6,8 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import sqlite3
 import os
+from sqlite3 import IntegrityError
+
 con = None
 
 class GooglePlusScraperPipeline(object):
@@ -29,27 +31,32 @@ class GooglePlusScraperPipeline(object):
     def createTable(self):
         self.cur.execute("CREATE TABLE IF NOT EXISTS google_plus_scraper_items(id INTEGER PRIMARY KEY NOT NULL, \
             title TEXT, \
-            link TEXT, \
+            link TEXT UNIQUE, \
             date TEXT \
             )")
+        self.cur.execute("CREATE INDEX IF NOT EXISTS idx_date ON google_plus_scraper_items(date)")
     
     def process_item(self, item, spider):
         self.storeInDb(item)
         return item
 
     def storeInDb(self,item):
-        self.cur.execute("INSERT INTO google_plus_scraper_items(\
-            title, \
-            link, \
-            date \
-            ) \
-        VALUES( ?, ?, ?)", \
-        ( \
-            item.get('title',''),
-            item.get('link',''),
-            item.get('date','')
-        ))
-        print '------------------------'
-        print 'Data Stored in Database'
-        print '------------------------'
-        self.con.commit()
+        try:
+            self.cur.execute("INSERT INTO google_plus_scraper_items(\
+                title, \
+                link, \
+                date \
+                ) \
+            VALUES( ?, ?, ?)", \
+            ( \
+                item.get('title',''),
+                item.get('link',''),
+                item.get('date','')
+            ))
+            print '------------------------'
+            print 'Data Stored in Database'
+            print '------------------------'
+            self.con.commit()
+        except IntegrityError:
+            print "Integrity Error when insert article to db : " + item.get('link')
+            self.con.rollback()
